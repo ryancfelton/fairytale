f2d852b937280ceeb6d0d817ea367952fa1e9093
ryancfelton <79480842+ryancfelton@users.noreply.github.com>
2024-07-17 22:15:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function checkPassword() {
     const password = document.getElementById('password').value;
     const error = document.getElementById('error');
-    const correctPassword = '123'; // Replace with your desired password
+    const correctPassword = 'Angela'; // Replace with your desired password
 
     if (password === correctPassword) {
         window.location.href = 'secret.html'; // Redirect to the new page
     } else {
-        error.textContent = 'Incorrect password. Please try again.';
+        error.textContent = 'Incorrect password. Please try again. What's your first name Senorita?';
     }
 }
