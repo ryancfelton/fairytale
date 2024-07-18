afbafdded16f2d1853694af9902ec1242fab60ec
ryancfelton <79480842+ryancfelton@users.noreply.github.com>
2024-07-17 21:09:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function checkPassword() {
     const password = document.getElementById('password').value;
     const error = document.getElementById('error');
-    const correctPassword = 'password'; // Replace with your desired password
+    const correctPassword = '123'; // Replace with your desired password
 
     if (password === correctPassword) {
         window.location.href = 'secret.html'; // Redirect to the new page
